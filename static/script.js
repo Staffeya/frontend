ea54bf0ec929629325af8083dbdd7c62dccba587
Staffeya <rutkevicsviktors@gmail.com>
2024-11-26 20:48:44 +0200

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,11 @@
+﻿const API_URL = "https://your-backend-url.com/api";
+
+document.getElementById('start').addEventListener('click', () => {
+    fetch(`${API_URL}/users`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert(`Найдено ${data.length} пользователей!`);
+        })
+        .catch(error => console.error('Ошибка:', error));
+});
